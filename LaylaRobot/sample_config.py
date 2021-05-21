a74c774880544c3983c7cd4d70e10a306836a754
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-05-21 11:22:37 +0530

    Update sample_config.py

--- LaylaRobot/sample_config.py
+++ LaylaRobot/sample_config.py
@@ -19,8 +19,8 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1777644370  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "GarimaQueen"
-    SUPPORT_CHAT = '@tinder_Queens'  #Your own group for support, do not add the @
+    OWNER_USERNAME = "@ram_of_telegram"
+    SUPPORT_CHAT = '@unitedbotsupport'  #Your own group for support, do not add the @
     JOIN_LOGGER = -1001447414063  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -1001447414063 #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
